992f7d298bc3fa4ed53fd274d0cc82005dcaa321
Shubham Naugai <71812538+ShubhuNaugai@users.noreply.github.com>
2023-04-23 20:06:10 +0530

    Update L_283 Move Zeroes-(Python)

--- LeetCode/Array/L_283_Move_Zeroes.py
+++ LeetCode/Array/L_283_Move_Zeroes.py
@@ -1,26 +1,41 @@
 """
 
-Given a binary array nums, return the maximum number of consecutive 1's in the array.
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
 
  
 
 Example 1:
 
-Input: nums = [1,1,0,1,1,1]
-Output: 3
-Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
 Example 2:
 
-Input: nums = [1,0,1,1,0,1]
-Output: 2
+Input: nums = [0]
+Output: [0]
  
 
 Constraints:
 
-1 <= nums.length <= 105
-nums[i] is either 0 or 1.
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
 
 
 """
 
-print("Hello World")
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        j = 0
+        for i in range(n):
+            if nums[i] != 0:
+                nums[j] = nums[i]
+                j += 1
+        
+        for z in range(j, n):
+            nums[z] = 0
+        return
