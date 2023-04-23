2833b026b7643913371eea65129bfc749e876b7e
Shubham Naugai <71812538+ShubhuNaugai@users.noreply.github.com>
2023-04-23 20:07:08 +0530

    Update L_283_Move_Zeroes.cpp

--- LeetCode/Array/L_283_Move_Zeroes.cpp
+++ LeetCode/Array/L_283_Move_Zeroes.cpp
@@ -1,41 +1,42 @@
 /*
 
-Given a binary array nums, return the maximum number of consecutive 1's in the array.
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
 
  
 
 Example 1:
 
-Input: nums = [1,1,0,1,1,1]
-Output: 3
-Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
 Example 2:
 
-Input: nums = [1,0,1,1,0,1]
-Output: 2
+Input: nums = [0]
+Output: [0]
  
 
 Constraints:
 
-1 <= nums.length <= 105
-nums[i] is either 0 or 1.
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
 
 */
 
 class Solution {
 public:
-    int findMaxConsecutiveOnes(vector<int>& nums) {
-        int curr = 0; 
-        int maxi = 0;
-        for (int i=0; i<nums.size(); i++){
-            if(nums[i]==1){
-                curr++; 
-            }
-            else if(nums[i]==0){
-                maxi = max(maxi, curr); 
-                curr = 0; 
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int j = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] != 0){
+                nums[j] = nums[i];
+                j++;  
             }
         }
-        return max(maxi, curr);
+        for(int z=j; z<n; z++){
+            nums[z] = 0; 
+        }
+        return;
     }
 };
