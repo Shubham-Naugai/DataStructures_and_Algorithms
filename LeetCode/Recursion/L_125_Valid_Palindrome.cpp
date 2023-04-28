ce19c040d15a4252e9a6a84451743cc577b1b488
Shubham Naugai <shubhunaugai1227@gmail.com>
2023-04-28 22:51:47 +0530

    Create L_125 Valid Palindrome-(C++)

---
+++ LeetCode/Recursion/L_125_Valid_Palindrome.cpp
@@ -0,0 +1,57 @@
+/*
+(Recursive-approach)
+
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+
+*/
+
+class Solution {
+public:
+    bool rec(int i, int n, string strr){
+        if (i >= n/2){
+            return true; 
+        }
+        if (strr[i] != strr[n-i-1]){
+            return false; 
+        }
+        return rec(i+1, n, strr);
+    }
+
+    bool isPalindrome(string s) {
+        string strr; 
+        for (int x=0; x < s.size(); x++){
+            if (isalnum(s[x])){
+                strr += (tolower(s[x])); 
+            }
+        }
+        int n = strr.size(); 
+        return rec(0, n, strr); 
+    }
+};
