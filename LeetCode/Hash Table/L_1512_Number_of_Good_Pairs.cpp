5dda47e1e26202de3cf36d34a71b30813e367337
Shubham Naugai <shubhunaugai1227@gmail.com>
2023-04-29 23:10:04 +0530

    Create L_1512 Number of Good Pairs-(C++)

---
+++ LeetCode/Hash Table/L_1512_Number_of_Good_Pairs.cpp
@@ -0,0 +1,52 @@
+/*
+
+
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+Example 2:
+
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+
+*/
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int, int>repeat;
+        
+        // number of good pairs
+        int num = 0;
+
+        for(int i=0; i<nums.size(); i++){
+            if (repeat.find(nums[i]) != repeat.end()){
+                num += repeat[nums[i]];
+                repeat[nums[i]] += 1; 
+            }
+            else{
+                repeat[nums[i]] = 1; 
+            }
+        }
+        return num; 
+    }
+};
