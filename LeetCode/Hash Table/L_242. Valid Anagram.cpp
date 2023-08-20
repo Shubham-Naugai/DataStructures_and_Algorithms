34158fe437179f9bb1552b6ed91d0f34e3a83957
Shubham <shubhunaugai1227@gmail.com>
2023-08-20 19:17:38 +0530

    Create L_242. Valid Anagram-(C++)

---
+++ LeetCode/Hash Table/L_242. Valid Anagram.cpp
@@ -0,0 +1,46 @@
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+*/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+
+        if (m != n){
+            return false;
+        }
+
+        unordered_map<int, int> maps;
+        for(int i=0; i<m; i++){
+            maps[s[i]]++;
+        }
+
+        unordered_map<int, int> mapt;
+        for(int j=0; j<n; j++){
+            mapt[t[j]]++;
+        }
+
+        return maps == mapt;
+    }
+};
