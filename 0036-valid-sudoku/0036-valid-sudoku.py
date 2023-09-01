d8ea86ec5d8f5a32c7541931b308087bcecdeca4
Shubham Naugai <71812538+ShubhuNaugai@users.noreply.github.com>
2023-09-02 01:45:57 +0530

    Time: 98 ms (86.36%), Space: 16.2 MB (97.08%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,5 +1,34 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # key = col index, value = set of values in that column
+        cols = collections.defaultdict(set) # {key: {}}
+        # key = row index, value = set of values in that row
+        rows = collections.defaultdict(set)
+        # key = pair of square's row index and col index and value = set of values in that square
+        squares = collections.defaultdict(set)  # key = (r /3, c /3)
+
+        d = {}
+        d[0] = {'5'}
+        print(d)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (
+                    board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]
+                ):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
+        return True
+
+
+        """
         # brute force
         def isValid(row, col, value, board):
             for i in range(9):
@@ -23,3 +52,4 @@ class Solution:
                         board[i][j] = temp
             return True
         return solve(board)
+        """
